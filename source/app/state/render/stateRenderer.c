60524adb30f9d032c1d7c8c82458931d310817c7
Luc van den Brand <LucvandenBrand@protonmail.com>
2019-01-25 22:53:25 +0100

    Beginning of state renderer.

--- source/app/state/render/stateRenderer.c
+++ source/app/state/render/stateRenderer.c
@@ -1,29 +1,25 @@
-#include "../state.h"
-
-#define NUM_SPRITES 1;
-OBJ_ATTR sprites[NUM_SPRITES];
+#include "./stateRenderer.h"
 
 OBJ_ATTR playerToSpriteObject(Player player) {
     u32 tileID = 0;
     u32 paletteBank = 0;
     OBJ_ATTR spriteObject;
-    setSpriteObjectAttributes(spriteObject,
+    setSpriteObjectAttributes(&spriteObject,
                               ATTR0_SQUARE,
                               ATTR1_SIZE_16,
                               ATTR2_PALBANK(paletteBank) | tileID);
-
     ObjectPoint position;
-    position.x = player.position.tileX;
-    position.y = player.position.tileY;
-    setSpriteObjectPosition(spriteObject, position);
+    position.x = player.position.tileX * 16;
+    position.y = player.position.tileY * 16;
+    setSpriteObjectPosition(&spriteObject, position);
 }
 
 void initializeStateRenderer(State state) {
     loadSpriteSheet();
     initializeSpriteObjectMemory(sprites, NUM_SPRITES);
-    sprites[0] = playerToSpriteObject(player);
+    sprites[0] = playerToSpriteObject(state.player);
     copySpriteObjectsToMemory(sprites, NUM_SPRITES);
-    setSpritesOnScreen():
+    setSpritesOnScreen();
 }
 
 void renderState(State oldState, State currentState, u32 transitionFrame, u32 currentFrame) {
