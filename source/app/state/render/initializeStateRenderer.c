c884f3d1373d3b9fe423c863eaf146b60468a73b
Luc van den Brand <LucvandenBrand@protonmail.com>
2019-01-26 00:37:10 +0100

    Added player and made sprite sheet configurable.

--- source/app/state/render/initializeStateRenderer.c
+++ source/app/state/render/initializeStateRenderer.c
@@ -1,7 +1,8 @@
 #include "stateRenderer.h"
+#include "playerSprite.h"
 
 void initializeStateRenderer(State state) {
-    loadSpriteSheet();
+    loadSpriteSheet(playerSpritePalette, PLAYER_PAL_LEN, playerSpriteTileSet, PLAYER_TILE_LEN);
     initializeSpriteObjectMemory(sprites, NUM_SPRITES);
     sprites[0] = playerToSpriteObject(state.player);
     copySpriteObjectsToMemory(sprites, NUM_SPRITES);
