49cc8f03ea32f83a2922d4302a71c66076c1a2d5
Luc van den Brand <LucvandenBrand@protonmail.com>
2019-01-26 16:25:13 +0100

    Fixed sprite compile error.

---
+++ source/assets/sprites/bladder/bladder01.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(bladder01)
+
+//======================================================================
+//
+//	bladder01, 32x32@4, 
+//	+ palette 256 entries, not compressed
+//	+ 16 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 512 = 1024
+//
+//	Time-stamp: 2019-01-26, 16:16:57
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BLADDER01_H
+#define GRIT_BLADDER01_H
+
+#define bladder01TilesLen 512
+extern const unsigned int bladder01Tiles[128];
+
+#define bladder01PalLen 512
+extern const unsigned short bladder01Pal[256];
+
+#endif // GRIT_BLADDER01_H
+
+//}}BLOCK(bladder01)
