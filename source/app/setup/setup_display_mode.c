d740eeab829fd6ef255f5bb1f96c9c7d89bbed30
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-26 20:50:30 +0100

    restart at smallest level.

--- source/app/setup/setup_display_mode.c
+++ source/app/setup/setup_display_mode.c
@@ -1,4 +1,6 @@
 #include "setup.h"
 #include "tonc_core.h"
 
-void setupDisplayMode() { REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_BG3; }
+void setupDisplayMode() {
+  REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_BG3;
+}
