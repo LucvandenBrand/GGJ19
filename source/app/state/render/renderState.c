5b0f63c450fe13dd5f50c5fb19a3acc3ee68d895
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-26 09:16:41 +0100

    proper map movement which is normalized with player position

--- source/app/state/render/renderState.c
+++ source/app/state/render/renderState.c
@@ -4,9 +4,9 @@ double easeInOutQuad(double t) {
     return t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1;
 }
 
-ObjectPoint interpolatePlayerPos(State *oldState, State *currentState,
+BackgroundPoint interpolatePlayerPos(State *oldState, State *currentState,
                                  double deltat, StateMode stateMode) {
-    ObjectPoint playerPos;
+    BackgroundPoint playerPos;
     s32 newX = currentState->player.position.tileX * 8;
     s32 newY = currentState->player.position.tileY * 8;
 
@@ -14,8 +14,8 @@ ObjectPoint interpolatePlayerPos(State *oldState, State *currentState,
         s32 oldX = oldState->player.position.tileX * 8;
         s32 oldY = oldState->player.position.tileY * 8;
 
-        playerPos.x = oldX + ((double)(newX - oldX) * deltat);
-        playerPos.y = oldY + ((double)(newY - oldY) * deltat);
+        playerPos.x = (oldX + ((double)(newX - oldX) * deltat));
+        playerPos.y = (oldY + ((double)(newY - oldY) * deltat));
         /* tte_printf("#{el}#{X}%d", deltat); */
     } else {
         playerPos.x = newX;
@@ -31,13 +31,18 @@ void renderState(State oldState, State currentState, u32 transitionFrame,
     /* playerPos.y = currentState.player.position.tileY * 16; */
     double deltat =
         (double)(currentFrame - transitionFrame) / (double)TRANSITION_TIME;
-    ObjectPoint playerPos = interpolatePlayerPos(
+    BackgroundPoint playerPos = interpolatePlayerPos(
         &oldState, &currentState, easeInOutQuad(deltat), stateMode);
     sprites[0] = playerToSpriteObject(
         (ObjectPoint){.x = SCREEN_WIDTH / 2, .y = SCREEN_HEIGHT / 2});
+    /* BackgroundPoint mapPos = playerPos; */
+    /* mapPos.x = -playerPos.x; */
+    /* mapPos.y = -playerPos.y; */
     BackgroundPoint mapPos;
     mapPos.x = -playerPos.x;
     mapPos.y = -playerPos.y;
+    mapPos.x -= SCREEN_WIDTH/2;
+    mapPos.y -= SCREEN_HEIGHT/2;
     shiftMap(map, mapPos);
     copySpriteObjectsToMemory(sprites, NUM_SPRITES);
 }
