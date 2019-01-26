04ab8e9a4d0255b4abd9cf3a9e3de12b82f1eead
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-26 09:05:06 +0100

    index tilemap properly

--- source/app/level/fill_tilemap.c
+++ source/app/level/fill_tilemap.c
@@ -10,8 +10,27 @@ uint se_index_fast(uint tx, uint ty, u16 bgcnt) {
 }
 
 void fillTilemap(Tilemap *tilemap, GenMap *genMap) {
-  for (unsigned int i = 0; i < 64 * 64; ++i) (*tilemap)[i] = 3;
+  /* for (unsigned int i = 0; i < 64 * 64; ++i) { */
+  for(int x = 0; x < MAP_WIDTH; ++x) {
+    for(int y = 0; y < MAP_HEIGHT; ++y) {
+      unsigned short tileImg = 0;
+      switch(genMap->ground[y * MAP_WIDTH + x]) {
+        case Empty:
+          tileImg = 2 * 20 + 3;
+          break;
+        case Wall:
+          tileImg = 1 * 20 + 3;
+          break;
+        default:
+          tileImg = 3;
+          break;
+      }
 
-  (*tilemap)[se_index_fast(2, 3, 0)] = 2 * 20 + 3;
-  (*tilemap)[se_index_fast(2, 2, 0)] = 1 * 20 + 3;
+        *tilemap[se_index_fast(x, y, 0)] = tileImg;
+    }
+  }
+    /* (*tilemap)[i] = genMap->ground[i]; */
+
+  /* *tilemap[se_index_fast(2, 3, 0)] = 2 * 20 + 3; */
+  /* *tilemap[se_index_fast(2, 2, 0)] = 1 * 20 + 3; */
 }
