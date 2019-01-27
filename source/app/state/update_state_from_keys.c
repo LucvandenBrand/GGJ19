a9504ff86679fd5c2780a8722fe098b1de41e550
troido <troido@protonmail.com>
2019-01-27 11:19:47 +0100

    alcohol/bladder still change when walking agains wall; alcohol time down

--- source/app/state/update_state_from_keys.c
+++ source/app/state/update_state_from_keys.c
@@ -3,21 +3,21 @@
 #include "tonc_input.h"
 
 State updateStateFromKeys(State state, Level *level, Map *map) {
-    State newState = state;
     if (!state.player.isSliding) {
-        newState.player.bladder += 1;
+        state.player.bladder += 1;
 
         if (state.player.inebriationSteps) {
-            --newState.player.inebriationSteps;
-            newState.player.velocity.tileX = -key_tri_vert();
-            newState.player.velocity.tileY = key_tri_horz();
+            --state.player.inebriationSteps;
+            state.player.velocity.tileX = -key_tri_vert();
+            state.player.velocity.tileY = key_tri_horz();
         } else {
-            newState.player.velocity.tileX = key_tri_horz();
-            newState.player.velocity.tileY = key_tri_vert();
+            state.player.velocity.tileX = key_tri_horz();
+            state.player.velocity.tileY = key_tri_vert();
         }
     }
-    newState.player.position.tileX += newState.player.velocity.tileX;
-    newState.player.position.tileY += newState.player.velocity.tileY;
+    State newState = state;
+    newState.player.position.tileX += state.player.velocity.tileX;
+    newState.player.position.tileY += state.player.velocity.tileY;
 
     if (isPlayerOnToilet(newState, level)) {
         newState.hasPlayerWon = true;
@@ -34,7 +34,7 @@ State updateStateFromKeys(State state, Level *level, Map *map) {
                      newState.player.position.tileY, Empty);
     }
     if (tileUnderPlayer(newState, level) == Alcohol) {
-        newState.player.inebriationSteps = 20;
+        newState.player.inebriationSteps = 10;
         setLevelTile(level, map, newState.player.position.tileX,
                      newState.player.position.tileY, Empty);
     }
